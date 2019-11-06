c624c88a613a777b9e29190e90e62490a8d7c63f
StoneSSon <46514120+StoneSSon@users.noreply.github.com>
2019-11-06 11:38:48 +0900

    Lorena Kidnapped

--- Grade Hunter/Assets/SSY/CameraCon.cs
+++ Grade Hunter/Assets/SSY/CameraCon.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class CameraCon : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
